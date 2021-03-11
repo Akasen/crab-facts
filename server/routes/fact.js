8216ec20d42bab020de7e1032df647e65ee9b18b
Joshua 'Akasen' Faria <akasen@protonmail.com>
2021-03-11 16:09:17 -0500

    create routes for creating and retrieving facts

---
+++ server/routes/fact.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+
+const Fact = require('../models/Fact');
+
+//Routes
+
+// @route   GET /facts
+// @desc    Get Facts
+// @access  Public
+router.get('/', (req, res) => {
+    Fact.find()
+        .then(facts => res.json(facts))
+});
+
+// @route   POST /facts
+// @desc    Create A Fact
+// @access  Public
+router.post('/list_all_facts', (req, res) => {
+    const newFact = new Fact({
+        message: req.body.message
+    });
+
+    newFact.save().then(fact => res.json(fact)).catch((err) => console.log(err));
+});
+
+/*
+router.post('/', (req, res) => {
+    console.log(JSON.stringify(req.body));
+});
+*/
+
+router.get('/randomfact', (req, res) => {
+    Fact.countDocuments().exec(function (err, count) {
+        var random = Math.floor(Math.random() * count)
+
+        Fact.findOne().skip(random).exec().then(fact => res.json(fact))
+    });
+})
+
+module.exports = router;
